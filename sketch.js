2acdba1892c64bc302992d9cc82fc55d7c90c331
JORGE-MEGEREDO <114191444+JORGE-MEGEREDO@users.noreply.github.com>
2023-03-02 18:25:54 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -93,9 +93,11 @@ function draw() {
         swordGroup.setVelocityYEach(0);
         //Remova os comentários corretos da linha de código para exibir 'game over' (fim de jogo)
 
-         over = createSprite(880,9980,20,20);
-         over.addAnimation("gameover",gover);
-         over.scale=0.7
+        
+        
+        over = createSprite(180,280,20,20);
+        over.addAnimation("gameover",gover);
+        over.scale=0.7
 
        
 
